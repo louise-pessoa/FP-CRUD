bee63fda4eb4b906aa0d63a38971aabdeab8d99f
louise-pessoa <lupmedeiros07@gmail.com>
2025-05-06 16:44:44 -0300

    atualizando variaveis

--- teste1.py
+++ teste1.py
@@ -1,52 +1,49 @@
-dicionario_treinos = {}
+historico_treinos = []
 
 while True:
-    opcoesUsuario = int(input("Escolha uma ação:\n1-CREATE\n2-READ\n3-UPDATE\n4-DELETE\nDigite apenas o número correspondente à ação: "))
+    opcoes_usuario = int(input("Escolha uma ação:\n1-CREATE\n2-READ\n3-UPDATE\n4-DELETE\nDigite apenas o número correspondente à ação: "))
 
 #CREATE
-    if opcoesUsuario == 1:
+    if opcoes_usuario == 1:
         data = (input("Data do treino (exemplo: xx xx xxxx): "))
-        dicionario_treinos[data] = (input("Adicionar treino: "))
+        historico_treinos[data] = (input("Adicionar treino: "))
         data = input("")
-        for value in (dicionario_treinos):
+        for value in (historico_treinos):
             print(value)
 
-
 #READ
-    elif opcoesUsuario == 2:
-        if dicionario_treinos == []:
+    elif opcoes_usuario == 2:
+        if historico_treinos == []:
             print("A lista está vazia, não há o que mostrar.")
         else:
-            with open("historico-treinos-crossfit.txt","r", encoding="utf8") as historico:
+            with open("treinos-crossfit.txt","r", encoding="utf8") as historico:
                 ler_historico = historico.read()
                 print(ler_historico)
 
-
-
 #UPDATE
-    elif opcoesUsuario == 3:
+    elif opcoes_usuario == 3:
         indice = int(input("Digite o índice (a partir de 0) do elemento a ser atualizado: "))
-        if indice in range (len(dicionario_treinos)):
-            dicionario_treinos[indice] = input("Digite o novo elemento: ")
+        if indice in range (len(historico_treinos)):
+            historico_treinos[indice] = input("Digite o novo elemento: ")
             i=0
-            for i in range (len(dicionario_treinos)):
-                print(i,"-",dicionario_treinos[i])
+            for i in range (len(historico_treinos)):
+                print(i,"-",historico_treinos[i])
         else:
             print("Este índice não é válido!")
 
 #DELETE
-    elif opcoesUsuario == 4:
+    elif opcoes_usuario == 4:
         indice = int(input("Digite o índice (a partir de 0) do elemento a ser excluído: "))
-        if indice in range(len(dicionario_treinos)):
-            elementoRemovido = dicionario_treinos.pop(indice)
+        if indice in range(len(historico_treinos)):
+            elementoRemovido = historico_treinos.pop(indice)
             print(elementoRemovido, "foi removido da lista")
-            if dicionario_treinos == []:
+            if historico_treinos == []:
                 print("Lista atualizada: ")
             else:
                 i=0
                 print("Lista atualizada:")
-                for i in range (len(dicionario_treinos)):
-                    print(f"{i}- {dicionario_treinos[i]}")
+                for i in range (len(historico_treinos)):
+                    print(f"{i}- {historico_treinos[i]}")
         else:
             print("Este índice não é válido!")
 
